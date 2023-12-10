4660d7796e289638d6d36e454d07a00c87071aee
Devyash Saini <dysaini2004@gmail.com>
2023-12-11 00:00:22 +0530

    detection added

--- ALPHA.py
+++ ALPHA.py
@@ -1,10 +1,9 @@
-# author: Devyash Saini
+# author:Devyash Saini
 import cv2
 import face_recognition
 import time
 import json
 import os
-import numpy as np
 import pickle
 
 # with open("apiArchive.json", "r") as f:
@@ -15,14 +14,21 @@ class User():
         os.environ["QT_QPA_PLATFORM"] = "xcb"
         print("Authenticating...")
 
-        my_face_encoding = np.load("main.tds.npy")
+        if os.path.exists("models.pickle"):
+            print("--> Pickle Located")
+            with open("models.pickle", "rb") as model:
+                res = pickle.load(model)
+                my_face_encoding = res["Devyash.tds"]
+        else:
+            import numpy as np
+            my_face_encoding = np.load("main.tds.npy")
 
         start_time = time.time()
         found_me = False
 
         video_capture = cv2.VideoCapture(0)
 
-        wt = 5
+        wt = 5 if auth else 300
         while time.time() - start_time < wt:
             ret, frame = video_capture.read()
             frame = cv2.flip(frame, 1)
@@ -37,27 +43,49 @@ class User():
             
             face_encodings = face_recognition.face_encodings(frame, face_locations)
 
-            wt = 10
+            wt = 10 if auth else 300
             for face_encoding, face_location in zip(face_encodings, face_locations):
-                if found_me:
-                    return True
-                
-                results = face_recognition.compare_faces([my_face_encoding], face_encoding)
-                if results[0]:
-                    top, right, bottom, left = face_location
-                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
-                    found_me = True
-                else:
-                    top, right, bottom, left = face_location
-                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                if auth:
+                    if found_me:
+                        return True
+                    
+                    results = face_recognition.compare_faces([my_face_encoding], face_encoding)
+                    if results[0]:
+                        top, right, bottom, left = face_location
+                        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+                        found_me = True
+                    else:
+                        top, right, bottom, left = face_location
+                        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-                cv2.imshow('Press q to exit authentication...', frame)
+                    cv2.imshow('Press q to exit authentication...', frame)
+                else:
+                    results = face_recognition.compare_faces(list(res.values()), face_encoding)
+                    c = 0
+                    for i in results:
+                        if i:
+                            name = list(res.keys())[c]
+                            top, right, bottom, left = face_location
+                            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+                            cv2.putText(frame, name, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+                            face_locations.remove(face_location)
+                            break
+                        c += 1
+            
+            for i in face_locations:
+                top, right, bottom, left = i
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                cv2.putText(frame, "anjaana", (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+            cv2.imshow('Press q to exit authentication...', frame)
                 
-            if found_me:
+            if found_me and auth:
                 return True
             
-        if not found_me:
+        if not found_me and auth:
             return False
+        
+        if not auth:
+            return True
 
         video_capture.release()
         cv2.destroyAllWindows()
@@ -91,11 +119,19 @@ class User():
             t = str(time.time())
 
             if entered & 0xFF == ord('s'):
-                with open("models.pickle", "rb") as model:
-                    res = pickle.load(model)
-                    res = json.loads(res)
-                    res["./models/" + name + "." + t] = face_encodings[0]
+
+                if os.path.exists("models.pickle"):
+                    with open("models.pickle", "rb") as model:
+                        res = pickle.load(model)
+                        # print(res)
+                else:
+                    res = {}
+
+                res[name + '.tds'] = face_encodings[0]
+                
+                with open("models.pickle", "wb") as model:
                     pickle.dump(res, model)
+
                 trained = True
                 break
 
@@ -106,4 +142,4 @@ class User():
 
         video_capture.release()
         cv2.destroyAllWindows()
-        print("Training Complete! Your face model has been saved as " + name + "." + t + ".tds.npy in the models directory") if trained else print("Training Failed! Please try again.")
+        print(f"Training Complete! Your face model ({name + '.tds'}) has been saved without errors...") if trained else print("Training Failed! Please try again.")
--- main.py
+++ main.py
@@ -9,15 +9,14 @@ if len(args) > 1:
             res = User.train(args[2])
         else:
             print("Error: User.Name required to train\nUsage: python3 main.py --train <User.Name>")
+    elif args[1] == "--detect":
+        res = User.verify(False)
+        if res:
+            print("Program Exited with 0 errors.")
+        else:
+            print("Exit!")
     else:
         print(f"Error: Invalid argument ({args[1]})")
-    if args[1] == "--detect":
-        if len(args) > 2: 
-            res = User.verify(False)
-            if res:
-                print("Program Exited with 0 errors.")
-            else:
-                print("Exit!")
 else: 
     res = User.verify(True)
     if res:
